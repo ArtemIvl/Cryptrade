73e7edda9da83b8192751a8f2329667877bcf1e2
ArtemIvl <captainivliev@gmail.com>
2023-10-30 12:48:49 +0100

    Login and register form updated, header added

--- cryptradefront/src/App.js
+++ cryptradefront/src/App.js
@@ -2,21 +2,14 @@ import './App.css';
 import CryptocurrencyList from './CryptocurrencyData/CryptocurrencyList';
 import Header from './Header/Header';
 import RegistrationLoginForm from './UserManagement/RegistrationLoginForm';
-import { useState } from 'react';
 
 function App() {
-
-  const [formType, setFormType] = useState('login'); // Initial form type
-
-  const changeFormType = (type) => {
-    setFormType(type);
-  };
-
   
   return (
     <div className="App">
       <Header />
-      <RegistrationLoginForm formType={formType} onChangeFormType={changeFormType}/>
+      <CryptocurrencyList />
+      <RegistrationLoginForm />
     </div>
   );
 }
--- cryptradefront/src/Header/Header.js
+++ cryptradefront/src/Header/Header.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import './Header.css'
 
 function Header() {
   return (
-    <div className='header'>Cryptrade</div>
+    <div className='header'>
+      <div className='container-left'>
+        <span>Cryptrade</span>
+        <span>Cryptocurrencies</span>
+        <span>Mock Trading</span>
+      </div>
+      <div className='container-right'>
+        <span>Portfolio</span>
+        <span>Search Bar</span>
+        <span>Sign in</span>
+      </div>
+    </div>
   )
 }
 
